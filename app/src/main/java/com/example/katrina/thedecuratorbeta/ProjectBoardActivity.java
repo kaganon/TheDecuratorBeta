665e328e230a72cae09d36387e63e29c4d50cc17
Katrina Aganon <katrina.aganon@gmail.com>
2019-01-17 15:38:27 -0800

    add new project board activity

---
+++ app/src/main/java/com/example/katrina/thedecuratorbeta/ProjectBoardActivity.java
@@ -0,0 +1,13 @@
+package com.example.katrina.thedecuratorbeta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_board);
+    }
+}
